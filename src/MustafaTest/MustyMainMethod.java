af1671dffe37337cac6bc07a37234a6b6883539c
mustafamandiraci <mustafarya@yahoo.co.uk>
2024-08-08 20:11:11 -0400

    first test

---
+++ src/MustafaTest/MustyMainMethod.java
@@ -0,0 +1,12 @@
+package MustafaTest;
+
+public class MustyMainMethod {
+    public static void main(String[] args) {
+        int num=5;
+        int num2= 6;
+        int total=num+num2;
+        System.out.println(total);
+        System.out.println("that's right");
+
+    }
+}
